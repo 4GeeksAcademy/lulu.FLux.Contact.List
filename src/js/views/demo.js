3baf3ccd2ee7a340df94d5a9cfd1584fbf354bf5
Lulu828-cyber <146968254+Lulu828-cyber@users.noreply.github.com>
2024-03-25 20:33:23 +0000

    needWork

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -7,37 +7,94 @@ import "../../styles/demo.css";
 
 export const Demo = () => {
 	const { store, actions } = useContext(Context);
+	const {fullname, setFullName} = useState ("");
+	const {emailAddress, setEmailAddress} = useState ("");
+	const {phoneNumber, setPhoneNumber} = useState ("");
+	const {streetAddress, setStreetAddress} = useState ("");
+
+	const handleSubmit = e =>
+		e.preventDefault();//este no siempre hace falta con React
+		console.log(fullname);
+
+
+
+
+		/* actions.addContact(fullname,emailAddress,phoneNumber,streetAddress);
+		setFullName("");//limpiar input
+		setEmailAddress("");
+		setPhoneNumber("");
+		setStreetAddress(""); */
+
+
+
+
+
 
 	return (
 		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
+			<form>
+				<div className="mb-3">
+					<label htmlFor="inputName" className="form-label"><em>Full Name</em></label>
+					<input 
+					type="text"
+					className="form-control"
+					id="inputName"
+					aria-describedby="nameHelp"
+					value={fullname} //asignar valor
+					onChange={e => setFullName(e.target.value)}
+					placeholder="Jhon Wick"
+					/>
+				</div>
+
+				<div className="mb-3">
+					<label htmlFor="emailAddress" className="form-label"><em>Email</em></label>
+					<input 
+					type="email"
+					className="form-control"
+					id="inputEmail"
+					aria-describedby="emailHelp"
+					value={emailAddress} //asignar valor
+					onChange={e => setEmailAddress(e.target.value)}
+					placeholder="Jhon@noemail.com"
+					/>
+					
+				</div>
+				<div className="mb-3">
+					<label htmlFor="emailAddress" className="form-label"><em>Phone Number</em></label>
+					<input 
+					type="numbers"
+					className="form-control"
+					id="inputPhoneNumber"
+					aria-describedby="phoneNumberHelp"
+					value={phoneNumber} //asignar valor
+					onChange={e => setPhoneNumber(e.target.value)}
+					placeholder="666.222.222"
+					/>
+					
+				</div>
+				<div className="mb-3">
+					<label htmlFor="emailAddress" className="form-label"><em>Street Address</em></label>
+					<input 
+					type="text"
+					className="form-control"
+					id="streetAddress"
+					aria-describedby="addressHelp"
+					value={streetAddress} //asignar valor
+					onChange={e => setStreetAddress(e.target.value)}
+					placeholder="c/ 34 san jose spain"
+					/>
+					
+				</div>
+			</form>
+			<button type="submit" className="btn btn-primary">Save</button>
+			<br/>
+
+			<button onClick={() => console.log(fullname)}
+
+				  <Link to="/">
+				<button className="btn btn-link">or get back to contacts.</button>
 			</Link>
+		
 		</div>
 	);
 };
